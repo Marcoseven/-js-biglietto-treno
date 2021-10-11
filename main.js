7bc762f77816674b62fb0766abcdbe9eea838575
Marcoseven <marco.caliciotti3@gmail.com>
2021-10-11 13:39:13 +0200

    Creato file JavaScript
    Inseriti i commenti, due variabili const e le funzioni console.log

---
+++ main.js
@@ -0,0 +1,8 @@
+// chiediamo all'utente il numero di km che vuole percorrere e l'età
+const distance = prompt("Quanti km vuole percorrere?");
+const age = prompt("Quanti anni hai?");
+
+console.log("Numero utente:", distance + " km");
+console.log("Numero utente:", age + " anni");
+
+// calcolo del prezzo totale del biglietto, con le condizioni che per ogni km il prezzo è pari a 0.21 € e poi va applicato uno sconto del 20% per i minorenni mentre va applicato uno sconto del 40% per gli over 65
