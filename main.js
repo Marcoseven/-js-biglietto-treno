f35ef8ff2de76093ca12f8a3ba7382c737e8a28d
Marcoseven <marco.caliciotti3@gmail.com>
2021-10-11 17:56:20 +0200

    Aggiunte modifiche
    Inserite numerose modifiche, dalle variabili ai commenti

--- main.js
+++ main.js
@@ -1,26 +1,47 @@
-/* chiediamo all'utente il numero di km che vuole percorrere e l'età, e poi calcoliamo il prezzo totale del biglietto, con le condizioni che per ogni km il prezzo è pari a 0.21 € e poi va applicato uno sconto del 20% per i minorenni mentre va applicato uno sconto del 40% per gli over 65 */
+/* chiediamo all'utente il numero di km che vuole percorrere e l'età, 
+e poi calcoliamo il prezzo totale del biglietto, con le condizioni che per ogni km il prezzo è pari a 0.21 € 
+e poi va applicato uno sconto del 20% per i minorenni mentre va applicato uno sconto del 40% per gli over 65 anni
+*/
 
-// variabili const
+// variabili
 const distance = prompt("Quanti km vuole percorrere?");
 const userAge = prompt("Quanti anni hai?");
-const result = distance * "0.21";
+const ageUnder = 18;
+const ageOver = 65;
+var result = distance * "0.21";
 
-// risposte visionate su console
+// console
 console.log("Km che vuole percorrere l'utente:", distance + " km");
 console.log("Età dell'utente:", userAge + " anni");
-console.log("Prezzo biglietto standard:", result + " €");
 
-// calcoli sconti
-if (userAge < 18) {
-	var discount_first = 0.21 - (0.21 / 100) * 20;
+// istruzioni condizionali
+if (userAge < ageUnder) {
+	const under = result - result * 0.2;
+	document.getElementById(
+		"answers"
+	).innerHTML = `Km che vuole percorrere l'utente: ${distance} km; Età dell'utente: ${userAge} anni; Prezzo biglietto scontato del 20% per gli under 18 anni: ${under.toFixed(
+		2
+	)} €`;
 	console.log(
-		"Prezzo biglietto scontato per i minorenni: ",
-		discount_first.toFixed(2) + " €"
+		"Prezzo biglietto scontato del 20% per gli under 18 anni:",
+		under.toFixed(2) + " €"
 	);
-} else if (userAge >= 65) {
-	var discount_second = 0.21 - (0.21 / 100) * 40;
+} else if (userAge > ageOver) {
+	const over = result - result * 0.2;
+	document.getElementById(
+		"answers"
+	).innerHTML = `Km che vuole percorrere l'utente: ${distance} km; Età dell'utente: ${userAge} anni; Prezzo biglietto scontato del 40% per gli over 65 anni: ${over.toFixed(
+		2
+	)} €`;
 	console.log(
-		"Prezzo biglietto scontato per gli over 65 anni: ",
-		discount_second.toFixed(2) + " €"
+		"Prezzo biglietto scontato del 40% per gli over 65 anni:",
+		over.toFixed(2) + " €"
 	);
+} else {
+	document.getElementById(
+		"answers"
+	).innerHTML = `Km che vuole percorrere l'utente: ${distance} km; Età dell'utente: ${userAge} anni; Prezzo biglietto standard: ${result.toFixed(
+		2
+	)} €`;
+	console.log("Prezzo biglietto standard:", result + " €");
 }
